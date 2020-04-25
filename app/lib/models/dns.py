fe99ba4a024690128f19fcc5f6c91abb3d72f7a7
Pavel Tsakalidis <p@vel.gr>
2020-04-25 12:41:06 +0100

    Update db models

--- app/lib/models/dns.py
+++ app/lib/models/dns.py
@@ -4,16 +4,28 @@ from app import db
 class DNSZoneModel(db.Model):
     __tablename__ = 'dns_zones'
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=True, index=True, default=0)
     domain = db.Column(db.String(255), nullable=True, default='', index=True)
     base_domain = db.Column(db.String(255), nullable=True, default='', index=True)
     full_domain = db.Column(db.String(255), nullable=True, default='', index=True)
+    active = db.Column(db.Boolean, default=True, index=True)
+    forward_unmatched = db.Column(db.Boolean, default=True, index=True)
+
+    # Required in all models.
+    created_at = db.Column(db.DateTime, nullable=True)
+    updated_at = db.Column(db.DateTime, nullable=True)
+
+
+class DNSRecordModel(db.Model):
+    __tablename__ = 'dns_records'
+    id = db.Column(db.Integer, primary_key=True)
+    dns_zone_id = db.Column(db.Integer, nullable=True, index=True, default=0)
     ttl = db.Column(db.Integer, nullable=True, default=0)
     rclass = db.Column(db.String(32), nullable=True, default='', index=True)
     type = db.Column(db.String(32), nullable=True, default='', index=True)
     address = db.Column(db.String(255), nullable=True, default='', index=True)
     active = db.Column(db.Boolean, default=True, index=True)
     exact_match = db.Column(db.Boolean, default=True, index=True)
-    user_id = db.Column(db.Integer, nullable=True, index=True, default=0)
 
     # Required in all models.
     created_at = db.Column(db.DateTime, nullable=True)
@@ -27,10 +39,11 @@ class DNSQueryLogModel(db.Model):
     domain = db.Column(db.String(255), nullable=True, default='', index=True)
     rclass = db.Column(db.String(32), nullable=True, default='')
     type = db.Column(db.String(32), nullable=True, default='')
-    forwarded = db.Column(db.Boolean, default=False, index=True)
-    found = db.Column(db.Boolean, default=False, index=True)
     resolved_to = db.Column(db.String(128), nullable=True, default='', index=True)
+    found = db.Column(db.Boolean, default=False, index=True)
+    forwarded = db.Column(db.Boolean, default=False, index=True)
     dns_zone_id = db.Column(db.Integer, nullable=True, default=0, index=True)
+    dns_record_id = db.Column(db.Integer, nullable=True, default=0, index=True)
 
     # Required in all models.
     created_at = db.Column(db.DateTime, nullable=True)
