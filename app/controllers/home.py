a5e29f809e7ddd72b14a29ff486a3a77a6ba9e35
Pavel Tsakalidis <p@vel.gr>
2020-04-26 07:48:30 +0100

    Fix home page login requirement

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,13 +1,11 @@
 from flask import Blueprint
-from flask_login import current_user
+from flask_login import current_user, login_required
 from flask import render_template, redirect, url_for, flash, request
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 
 @bp.route('/', methods=['GET'])
+@login_required
 def index():
-    if not current_user.is_authenticated:
-        return redirect(url_for('auth.login'))
-
     return render_template('home/index.html')
