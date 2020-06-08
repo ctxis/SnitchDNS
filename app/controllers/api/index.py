9de99c17f9bff178cfdaba7e689a56d8626a60ae
Pavel Tsakalidis <p@vel.gr>
2020-06-08 12:30:45 +0100

    Fix Swagger link/content-type

--- app/controllers/api/index.py
+++ app/controllers/api/index.py
@@ -1,19 +1,21 @@
 from . import bp
 from flask import redirect, url_for, Response
+from flask_login import login_required
 from app.lib.api.base import ApiBase
 
 
 @bp.route('/', methods=['GET'])
+@login_required
 def index():
     return redirect(url_for('api.swagger'))
 
 
 @bp.route('/swagger.yaml', methods=['GET'])
+@login_required
 def swagger():
     base = ApiBase()
     contents = base.get_swagger_file('v1')
 
-    response = Response()
-    response.headers.add('Content-Type', 'text/plain')
+    response = Response(mimetype='text/plain')
     response.data = contents
     return response
