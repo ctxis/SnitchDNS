60e39e48a83459eb743c946f6ab9acd7d4acb74f
Pavel Tsakalidis <p@vel.gr>
2020-06-14 14:03:19 +0100

    Enable debug log by default on the DNS daemon

--- app/lib/daemon/daemon.py
+++ app/lib/daemon/daemon.py
@@ -6,6 +6,8 @@ from twisted.names import client
 from app.lib.daemon.server.resolver import DatabaseDNSResolver
 from app.lib.daemon.server.factory import DatabaseDNSFactory
 from app.lib.daemon.server.logging import DatabaseDNSLogging
+from twisted.python import log
+import logging
 
 
 class SnitchDaemon:
@@ -17,15 +19,22 @@ class SnitchDaemon:
 
     def start(self):
         app_for_context = create_app()
-        logging = DatabaseDNSLogging(app_for_context)
+        dns_logging = DatabaseDNSLogging(app_for_context)
 
-        clients = [DatabaseDNSResolver(app_for_context, Provider().dns_manager(), logging)]
+        clients = [DatabaseDNSResolver(app_for_context, Provider().dns_manager(), dns_logging)]
         if self.__forwarding_enabled and len(self.__forwarders) > 0:
             clients.append(client.Resolver(servers=self.__forwarders))
 
-        factory = DatabaseDNSFactory(clients=clients)
+        factory = DatabaseDNSFactory(clients=clients, verbose=2)
         factory.app = app_for_context
-        factory.logging = logging
+        factory.logging = dns_logging
+
+        observer = log.PythonLoggingObserver()
+        observer.start()
+
+        logging.basicConfig()
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
 
         reactor.listenUDP(self.__port, DNSDatagramProtocol(factory), interface=self.__host)
         reactor.listenTCP(self.__port, factory, interface=self.__host)
