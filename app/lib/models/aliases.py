ebadfc9181baa2b81318dda8d1699a0018e1dfb9
Pavel Tsakalidis <p@vel.gr>
2020-10-24 20:53:13 +0100

    Remove unique attribute from tag and alias models

--- app/lib/models/aliases.py
+++ app/lib/models/aliases.py
@@ -5,8 +5,8 @@ class AliasModel(db.Model):
     __tablename__ = 'aliases'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=True, index=True, default=0)
-    ip = db.Column(db.String(255), nullable=True, default='', unique=True)
-    name = db.Column(db.String(255), nullable=True, default='', unique=True)
+    ip = db.Column(db.String(255), nullable=True, default='', index=True)
+    name = db.Column(db.String(255), nullable=True, default='', index=True)
 
     # Required in all models.
     created_at = db.Column(db.DateTime, nullable=True)
--- app/lib/models/tags.py
+++ app/lib/models/tags.py
@@ -5,7 +5,7 @@ class TagModel(db.Model):
     __tablename__ = 'tags'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=True, index=True, default=0)
-    name = db.Column(db.String(255), nullable=True, default='', unique=True)
+    name = db.Column(db.String(255), nullable=True, default='', index=True)
 
     # Required in all models.
     created_at = db.Column(db.DateTime, nullable=True)
