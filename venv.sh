2bdaf6b9f5f9644528111fb165147fbd383ab171
Pavel Tsakalidis <p@vel.gr>
2020-12-06 10:23:09 +0000

    Update license and venv.sh env

--- venv.sh
+++ venv.sh
@@ -5,12 +5,19 @@
 VENV_SCRIPT="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd "$VENV_SCRIPT"
 
-. venv/bin/activate
-export FLASK_APP=app
-export FLASK_ENV=development
-
 # Load the SnitchDNS config environment variables.
 export $(grep -v '^#' data/config/env/snitch.conf)
+
+SNITCH_FLASK_ENVIRONMENT="production"
+if [[ -n "$SNITCHDNS_ENV" ]]; then
+  if [[ "$SNITCHDNS_ENV" == "development" ]] || [[ "$SNITCHDNS_ENV" == "production" ]]; then
+    SNITCH_FLASK_ENVIRONMENT="$SNITCHDNS_ENV"
+  fi
+fi
+
+. venv/bin/activate
+export FLASK_APP=app
+export FLASK_ENV="$SNITCH_FLASK_ENVIRONMENT"
 
 # And now execute whatever was passed through the command line.
 "$@"
