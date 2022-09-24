a4178f773daf7cd53cb6c0734685fbba90260945
Pavel Tsakalidis <p@vel.gr>
2022-09-24 08:44:54 +0100

    Implement Azure config page

--- app/controllers/config/__init__.py
+++ app/controllers/config/__init__.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 bp = Blueprint('config', __name__, url_prefix='/config')
 
 from . import index
-from .system import dns, ldap, password_complexity, smtp, system, users, webpush, slack, logs, teams, radius
+from .system import dns, ldap, password_complexity, smtp, system, users, webpush, slack, logs, teams, radius, azure
 from .account import profile, api
